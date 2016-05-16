e3bd1c8763392762b0f91f4d9857cf0272a0f61a
fukayatsu <fukayatsu@gmail.com>
2016-05-16 13:10:31 +0900

    Improve github compare link

--- lib/compare_linker/formatter/markdown.rb
+++ lib/compare_linker/formatter/markdown.rb
@@ -5,22 +5,25 @@ class CompareLinker
     class Markdown < Base
       def format(gem_info)
         g = OpenStruct.new(gem_info)
+        downgraded = downgrade?(g.old_ver, g.new_ver, g.old_tag, g.new_tag)
 
         text = "* [ ] "
         text += case
         when g.owner && g.old_rev && g.new_rev
-          "#{g.gem_name}: https://github.com/#{g.owner}/#{g.gem_name}/compare/#{g.old_rev}...#{g.new_rev}"
+          range = downgraded ? "#{g.new_rev}...#{g.old_rev}" : "#{g.old_rev}...#{g.new_rev}"
+          "#{g.gem_name}: https://github.com/#{g.owner}/#{g.gem_name}/compare/#{range}"
         when g.homepage_uri
           "[#{g.gem_name}](#{g.homepage_uri}): #{g.old_ver} => #{g.new_ver}"
         when g.old_tag && g.new_tag
-          "#{g.gem_name}: https://github.com/#{g.repo_owner}/#{g.repo_name}/compare/#{g.old_tag}...#{g.new_tag}"
+          range = downgraded ? "#{g.new_tag}...#{g.old_tag}" : "#{g.old_tag}...#{g.new_tag}"
+          "#{g.gem_name}: https://github.com/#{g.repo_owner}/#{g.repo_name}/compare/#{range}"
         when g.repo_owner && g.repo_name
           "[#{g.gem_name}](https://github.com/#{g.repo_owner}/#{g.repo_name}): #{g.old_ver} => #{g.new_ver}"
         else
           "#{g.gem_name}: (link not found) #{g.old_ver} => #{g.new_ver}"
         end
 
-        if downgrade?(g.old_ver, g.new_ver, g.old_tag, g.new_tag)
+        if downgraded
           text += " (downgrade)"
         end
 
