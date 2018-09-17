7d6560ca78ca59dba8c045c68fc4b9095cd60333
Takashi Masuda <masutaka.net@gmail.com>
2018-09-17 19:18:22 +0900

    tmp

--- lib/compare_linker/formatter/base.rb
+++ lib/compare_linker/formatter/base.rb
@@ -19,6 +19,8 @@ class CompareLinker
 
       def downgrade?(old_ver, new_ver, old_tag, new_tag, old_rev, new_rev)
 p :bbb
+p new_ver
+p old_ver
 p Gem::Version.new(new_ver) < Gem::Version.new(old_ver)
 p (old_tag && new_tag && Gem::Version.new(to_ver(new_tag)) < Gem::Version.new(to_ver(old_tag)))
 p (old_rev && new_rev && Gem::Version.new(to_ver(new_rev)) < Gem::Version.new(to_ver(old_rev)))
