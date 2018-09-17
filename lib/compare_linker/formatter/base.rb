6555ea35d21f3f363a95f969ca025a6ae78488ea
Takashi Masuda <masutaka.net@gmail.com>
2018-09-17 19:11:37 +0900

    tmp

--- lib/compare_linker/formatter/base.rb
+++ lib/compare_linker/formatter/base.rb
@@ -18,10 +18,15 @@ class CompareLinker
       end
 
       def downgrade?(old_ver, new_ver, old_tag, new_tag, old_rev, new_rev)
+p :bbb
+p Gem::Version.new(new_ver) < Gem::Version.new(old_ver)
+p (old_tag && new_tag && Gem::Version.new(to_ver(new_tag)) < Gem::Version.new(to_ver(old_tag)))
+p (old_rev && new_rev && Gem::Version.new(to_ver(new_rev)) < Gem::Version.new(to_ver(old_rev)))
         Gem::Version.new(new_ver) < Gem::Version.new(old_ver) ||
           (old_tag && new_tag && Gem::Version.new(to_ver(new_tag)) < Gem::Version.new(to_ver(old_tag))) ||
           (old_rev && new_rev && Gem::Version.new(to_ver(new_rev)) < Gem::Version.new(to_ver(old_rev)))
       rescue ArgumentError # Gem::Version raise ArgumentError if it is invalid as version
+p :ccc
         false
       end
 
